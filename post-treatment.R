e77fabec423a7d6fb5e93c1b967db130b7afb6dc
Eric Marcon <eric.marcon@agroparistech.fr>
2024-03-12 11:23:15 +0100

    added new case

--- post-treatment.R
+++ post-treatment.R
@@ -8,14 +8,25 @@ readLines("hdr.html") |>
     # Remplacement par un point-virgule
     replacement = '</span></strong>; <span class="citation">'
   ) |>
+  stringi::stri_replace_all_fixed(
+    # Motif à remplacer: fin de citation normale + citation grasse
+    pattern = ')</span>+<strong><span class="citation">(',
+    # Remplacement par un point-virgule
+    replacement = '</span>; <strong><span class="citation">'
+  ) |>
   # Fichier final
   writeLines(con = "hdr_final.html")
 
 # TeX ----
 readLines("hdr.tex") |>
   stringi::stri_replace_all_fixed(
-    # Motif à remplacer
+    # Motif à remplacer: fin de citation grasse + citation normale
     pattern = ')}+(',
     replacement = '}; '
+  ) |>
+  stringi::stri_replace_all_fixed(
+    # Motif à remplacer: fin de citation normale + citation grasse
+    pattern = ')+\\textbf{(',
+    replacement = '; \\textbf{'
   ) |>
   writeLines(con = "hdr_final.tex")
